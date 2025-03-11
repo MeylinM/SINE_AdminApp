e8d1011e81247a05047b69bcb6037539049ced6b
Meylin <meylinmoon@gmail.com>
2025-03-11 09:29:37 +0100

    Cambio para mostrar los datos en la tabla.

---
+++ services/productoService.js
@@ -0,0 +1,21 @@
+const API_URL = "http://192.168.10.101:3000/producto"; // Reemplaza con la IP de tu servidor
+
+// Obtener todas las bobinas desde la base de datos
+export const obtenerBobinas = async () => {
+  try {
+    console.log(`Haciendo petición a: ${API_URL}`);
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+
+    const data = await response.json();
+    console.log("Datos obtenidos:", data);
+    return data;
+  } catch (error) {
+    console.error("Error obteniendo bobinas:", error);
+    return [];
+  }
+};
+// Exportar función para que se pueda usar en `Bobinas.js`
+export default {
+    obtenerBobinas,
+  };
---
+++ services/usuarioProductoService.js
@@ -0,0 +1,22 @@
+const API_URL = "http://192.168.10.101:3000/usuario_producto"; // Reemplaza con la IP de tu servidor
+
+//Obtener empleados que se encargaron de las bobinas
+export const obtenerInforme = async () => {
+  try{
+    console.log(`Haciendo petición a: ${API_URL}`);
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+
+    const data = await response.json();
+    console.log("Datos obtenidos:", data);
+    return data;
+  }catch (error) {
+    console.error("Error obteniendo informe:", error);
+    return [];
+  }
+
+};
+// Exportar función para que se pueda usar en `Bobinas.js`
+export default {
+  obtenerInforme,
+};
